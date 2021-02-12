3929ee7534caccaea1a7cff428e1e9785e89e43e
Michael Kunynets <xfreed1@gmail.com>
2021-02-12 19:05:27 +0200

    Code formatting
    Yep I formatted your code

--- main.cpp
+++ main.cpp
@@ -3,12 +3,10 @@
 #include <time.h>
 using namespace std;
 
-int *fill_and_print(int width, int height, int **matrix)
+int* fill_and_print(int width, int height, int** matrix)
 {
-    for (int index_1 = 0; index_1 < height; index_1++)
-    {
-        for (int index_2 = 0; index_2 < width; index_2++)
-        {
+    for (int index_1 = 0; index_1 < height; index_1++) {
+        for (int index_2 = 0; index_2 < width; index_2++) {
             matrix[index_1][index_2] = rand() % 100;
             cout << matrix[index_1][index_2] << "  ";
         }
@@ -23,15 +21,13 @@ int main()
     cout << "Enter size of matrix ";
     cin >> height >> width;
 
-    if (width < 1 || height < 1)
-    {
+    if (width < 1 || height < 1) {
         cout << "Error... You enter wrong matrix size" << '\n';
         return 1;
     }
 
-    int **matrix = new int *[height];
-    for (int i = 0; i < height; ++i)
-    {
+    int** matrix = new int*[height];
+    for (int i = 0; i < height; ++i) {
         matrix[i] = new int[width];
     }
 
